20728245507699f6e656c221abbef7338d963627
Sp1n4ch16 <alvintolosa16@gmail.com>
2023-08-11 13:49:43 +0800

    Initial commit

---
+++ src/approve.js
@@ -0,0 +1,24 @@
+const {OnlineConsult} = require('../database/mongodb')
+
+const approvedAPI = async (req, res) => {
+    try {
+        //await client.connect();
+        //const db = client.db("<database-name>");
+        const { appointmentId } = req.body;
+    
+        // Retrieve the appointment to be canceled
+        const onlineConsult = await OnlineConsult.findOne({ _id: appointmentId });
+    
+        if (onlineConsult) {
+          (onlineConsult.status = "Approved"),
+            (onlineConsult.isVerified = true),
+            await onlineConsult.save();
+        } else {
+          res.send("No appointmentId match to be approve");
+        }
+        res.sendStatus(200);
+      } catch (error) {
+        console.error("Error canceling appointment:", error);
+        res.status(500).send("An error occurred");
+      }
+}
