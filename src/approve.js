523481ac66b9b5dc4cb4ee5072a8a249b09a3f13
Sp1n4ch16 <alvintolosa16@gmail.com>
2023-08-28 11:26:23 +0800

    update

--- src/approve.js
+++ src/approve.js
@@ -1,4 +1,5 @@
-const {OnlineConsult} = require('../database/mongodb')
+const {OnlineConsult, Appointment} = require('../database/mongodb')
+const transporter = require("../src/nodemailer");
 
 const approvedAPI = async (req, res) => {
     try {
@@ -8,12 +9,50 @@ const approvedAPI = async (req, res) => {
     
         // Retrieve the appointment to be canceled
         const onlineConsult = await OnlineConsult.findOne({ _id: appointmentId });
+        const appointment = await Appointment.findOne({ _id: appointmentId });
     
         if (onlineConsult) {
           (onlineConsult.status = "Approved"),
             (onlineConsult.isVerified = true),
             await onlineConsult.save();
-        } else {
+
+            var mailOptions = {
+              from: ' "Your Appointment is approved" <dummy8270@gmail.com>',
+              to: onlineConsult.email,
+              subject: "Dr. Ryan Pangilinan dental clinic",
+              html: `<h2> Your Online Consultation in Dr. Ryan Pangilinan dental clinic has been approved. You can now pay for the appointment to proceed.</h2>`,
+            };
+          
+            transporter.sendMail(mailOptions, function (error, info) {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log("cancel message has been sent to the patient");
+              }
+            });
+
+        } if (appointment) {
+          (appointment.status = "Approved"),
+          (appointment.isVerified = true),
+          await appointment.save();
+
+          
+          var mailOptions = {
+            from: ' "Your Appointment is approved" <dummy8270@gmail.com>',
+            to: appointment.email,
+            subject: "Dr. Ryan Pangilinan dental clinic",
+            html: `<h2> Your Online Consultation in Dr. Ryan Pangilinan dental clinic has been approved.</h2>`,
+          };
+        
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("cancel message has been sent to the patient");
+            }
+          });
+        }
+        else {
           res.send("No appointmentId match to be approve");
         }
         res.sendStatus(200);
